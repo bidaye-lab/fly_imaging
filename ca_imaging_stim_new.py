75c2550dff25208f4a9cd210e37843d86652246d
Nino Mancini <nino.mancini@mpfi.org>
2023-12-01 11:03:02 -0500

    initial commit new script

---
+++ ca_imaging_stim_new.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+# ---
+
+# %%
+import numpy as np
+import pandas as pd
+
+import matplotlib.pylab as plt
+import seaborn as sns
+from pathlib import Path
+import h5py
+from sklearn.linear_model import LinearRegression
+
+
+import utils as utl
+
+# %% [markdown]
+# test2
