efd1bdc50af94b84d46a73a40c546bd66eed958c
Nino Mancini <nino.mancini@mpfi.org>
2023-12-01 11:36:47 -0500

    added gitignore, selected different interpreter

--- ca_imaging_stim_new.py
+++ ca_imaging_stim_new.py
@@ -8,6 +8,10 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 # %%
